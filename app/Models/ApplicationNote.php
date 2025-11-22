e7d9b7644dba15d827a79e28dcc608ec46876032
wittyamber <bausingjuanamae@gmail.com>
2025-11-22 16:58:15 +0800

    audit-log

--- app/Models/ApplicationNote.php
+++ app/Models/ApplicationNote.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Traits\Auditable;
 
 class ApplicationNote extends Model
 {
     use HasFactory;
+    use Auditable;
 
     /**
      * The attributes that are mass assignable.
